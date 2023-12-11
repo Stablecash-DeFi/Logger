f116e1049ebd07b87a4de46edd0ef3dc03a68433
SCcagg5 <sccagg5@gmail.com>
2023-12-11 20:53:38 +0100

    update

--- Server/app.py
+++ Server/app.py
@@ -1,9 +1,12 @@
 from bottle import Bottle, request, response
 import os
+from pymongo import MongoClient
 
 app = Bottle()
 
-json_data = []
+client = MongoClient('mongodb://mongo:27017/') 
+db = client['json_db']
+collection = db['json_collection']
 
 @app.post('/')
 def receive_json():
@@ -12,14 +15,15 @@ def receive_json():
     if auth_token != f'Bearer {expected_token}':
         response.status = 401
         return {'error': 'Unauthorized'}
+
     data = request.json
     if data:
         if isinstance(data, list):
             for i in range(len(data)):
-                json_data.append(data[i])
+                collection.insert_one({'json_data': data[i]})
         else:
-            json_data.append(data)
-        return {'message': None}
+            collection.insert_one({'json_data': data})
+        return {'error': None}
     else:
         return {'error': 'Missing data'}
 
@@ -31,7 +35,11 @@ def get_all_json():
     if auth_token != f'Bearer {expected_token}':
         response.status = 401
         return {'error': 'Unauthorized'}
-    return {'json_data': json_data}
+
+    result = []
+    for document in collection.find():
+        result.append(document['json_data'])
+    return {'json_data': result}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
