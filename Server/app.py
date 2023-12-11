011a8a1de43289e5d1bdd3078590a4001e135bc5
SCcagg5 <sccagg5@gmail.com>
2023-12-11 22:08:17 +0100

    update

--- Server/app.py
+++ Server/app.py
@@ -46,7 +46,7 @@ def get_all_json():
     for document in collection.find():
         result.append(document['json_data'])
     collection.delete_many({})
-    return {'json_data': result}
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
