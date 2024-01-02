46c43dbde5b9c7fbfb1cec3016442d9a5e5a1ff3
SCcagg5 <sccagg5@gmail.com>
2024-01-02 23:38:47 +0100

    Update app.py

--- Client/app.py
+++ Client/app.py
@@ -2,6 +2,7 @@ import http.client
 import json
 import time
 import csv
+import os
 from pathlib import Path
 from typing import Dict, Any, List
 
@@ -94,28 +95,30 @@ def main():
 
     if url is None or token is None:
         exit(1)
-  
+
     headers = {
         'authorization': "Bearer {token}",
-        'cache-control': "no-cache"
     }
-    json_filename = 'data.json'
+    json_filename = '/data/data.json'
     max_records = 50000
 
     fetcher = DataFetcher(url, headers)
     json_manager = JSONFileManager(json_filename)
 
     data = fetcher.fetch()
+    if data == {"error": "Unauthorized"}:
+        print("error")
+        exit(1)
     current_data = json_manager.load_data()
     current_data.append(data)
-  
+
     if len(current_data) >= max_records:
-        csv_filename = f"data_{int(time.time())}.csv"
+        csv_filename = f"/data/data_{int(time.time())}.csv"
         CSVConverter.convert(current_data[:max_records], csv_filename)
         current_data = current_data[max_records:]
         if not current_data:
             current_data = []
-          
+
     json_manager.save_data(current_data)
     time.sleep(300)
 
